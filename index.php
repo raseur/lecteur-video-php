44b25fbb2b54da47a034d01167ce9ddf18e25ae0
Antoine J <64862896+raseur@users.noreply.github.com>
2023-01-09 18:15:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 
+<style>
+  .video-container {
+    width: 50%;
+    float: left;
+  }
+</style>
+
+<div class="video-container">
+  <video width="320" height="240" controls>
+    <source src="video.mp4" type="video/mp4">
+    Le navigateur ne supporte pas ce format de vidéo.
+  </video>
+</div>
+
+<div class="video-container">
+  <!-- autre contenu de votre site web -->
+</div>
